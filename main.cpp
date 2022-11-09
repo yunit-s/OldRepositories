b613eb3974826539b803048b2c4844f26e61a19d
yunit <yung9031@gmail.com>
2022-11-09 19:58:30 +0900

    221109 Bkjn_24174

--- main.cpp
+++ main.cpp
@@ -1,9 +1,39 @@
-//first commit
+/*
+// 문제번호-문제이름
+
+// 구현 방법
+
+
+// 변수 설명
+
+
+*/
 
 #include <iostream>
 
 using namespace std;
 
 int main(void) {
+    //input
+
+    //process
+
+    //output
+
     return 0;
 }
+
+
+/*
+
+// 제출 결과
+구현 시간 - 
+1번째 시도 -
+
+// test case
+
+
+// test case 정답
+
+
+*/
