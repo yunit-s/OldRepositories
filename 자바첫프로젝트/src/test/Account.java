6e2549cb06f981e6e47d44064e2a3c513062844e
yunit <yung9031@gmail.com>
2022-11-29 20:51:17 +0900

    First commit

---
+++ 자바첫프로젝트/src/test/Account.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Account {
+
+	// method이면서 아주 특수한 기능을 하는 method
+	// 전체 프로젝트의 entry point
+	public static void main(String args[]) {
+		// 이 안의 코드가 실행돼요!
+		System.out.println("이제 고만해요.. 너무 힘들어요!"); // 표준 출력에 내가 원하는 문자열을 출력
+		
+	}
+	
+}
